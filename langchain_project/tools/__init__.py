291aecf5afad7a3230d5c020275d3690e8183800
jvictor96 <jose.macedo@marlo.com.au>
2026-02-26 14:39:39 -0300

    it is local now

--- langchain_project/tools/__init__.py
+++ langchain_project/tools/__init__.py
@@ -3,11 +3,9 @@ Tools package for the LangChain chatbot.
 """
 
 from .git_tools import (
-    run_git_diff_in_other_directory,
-    run_git_diff_between_branches_in_other_directory
+    run_git_diff_between_branches
 )
 
 tools = [
-    run_git_diff_in_other_directory,
-    run_git_diff_between_branches_in_other_directory,
+    run_git_diff_between_branches,
 ]
